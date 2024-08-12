02ced052cc48ea7f5448f4404a20a48335092b40
Ruslan Ponomarenko <ruponi@gmail.com>
2024-08-12 15:51:51 -0700

    Initial Commit

---
+++ PageProgress/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageProgress
+//
+//  Created by Ruslan Ponomarenko on 8/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
